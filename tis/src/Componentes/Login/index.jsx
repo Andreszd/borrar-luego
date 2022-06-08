dfb54bb551f5e67a786531afe1384948fe6eada4
andrescj <contretaa@gmail.com>
2022-06-07 20:44:39 -0400

    refactor routes protection
    Signed-off-by: andrescj <contretaa@gmail.com>

--- tis/src/Componentes/Login/index.jsx
+++ tis/src/Componentes/Login/index.jsx
@@ -1,72 +1,74 @@
-import './login.css'
+import './login.css';
 
-import {useState, useContext} from 'react'
+import { useState, useContext } from 'react';
 
-import {AuthContext} from '../../store/user'
-import { useNavigate } from "react-router-dom";
-function LoginUsr(){
-    let navigate = useNavigate();
+import { AuthContext } from '../../store/user';
+function LoginUsr() {
+  const { signIn } = useContext(AuthContext);
 
-    const {signIn, isAdmin} = useContext(AuthContext)
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [userForm, setUserForm] = useState({})
-    const [isLoading, setIsLoading] = useState(false)
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
 
-    const handleOnChange = (evt) => {}
+    const body = {
+      email: evt.target.email.value,
+      password: evt.target.password.value,
+    };
 
-    const handleSubmit = async(evt) => {
-        evt.preventDefault()
-        
-            const user = {
-                'email': evt.target.email.value,
-                'password': evt.target.password.value
-            }
-            
-            setIsLoading(true)
-            signIn(user).then(res => {
-                if (isAdmin){
-                    setIsLoading(false)
-                    navigate("/Home-admin");
-                }else{
-                    navigate("/");
-                }
-               
-            })
-        
+    setIsLoading(true);
+    try {
+      await signIn(body);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
     }
+  };
 
-    return(
-        <div className="container cont_form">
-            <form onSubmit={handleSubmit}>
-                <div className={`formulario ${isLoading && 'contanier-loading'}`}
-                id='login_form_cont'>
-                    <h1 style={{textAlign:"center"}}>Inicio de Sesi&oacute;n</h1>
-                    <p>Ingrese su correo</p>
-                    <p> electronico registrado</p>
-                    <div className="grupo">
-                        <input className="input_form" 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        onChange={handleOnChange}  
-                        required /><span className="barra"></span>
-                        <label className="label_form" htmlFor="">Email</label>
-                    </div>
-                    <div className="grupo">
-                        <input className="input_form" 
-                            onChange={handleOnChange}
-                            type="password" name="password" id="passwprd" required 
-                        /><span className="barra"></span>
-                        <label className="label_form" htmlFor="">Contraseña</label>
-                    </div>
-                    <div  className="boton_form" id='btn_login'>
-                        <button type="submit" >Iniciar Sesi&oacute;n</button>
-                    </div>
-                    <br />
-                    <a href="">¿Se te olvido la contraseña?</a>
-                </div>
-            </form>
-        </div>    
-)}
+  return (
+    <div className='container cont_form'>
+      <form onSubmit={handleSubmit}>
+        <div
+          className={`formulario ${isLoading && 'contanier-loading'}`}
+          id='login_form_cont'>
+          <h1 style={{ textAlign: 'center' }}>Inicio de Sesi&oacute;n</h1>
+          <p>Ingrese su correo</p>
+          <p> electronico registrado</p>
+          <div className='grupo'>
+            <input
+              className='input_form'
+              type='email'
+              name='email'
+              id='email'
+              required
+            />
+            <span className='barra'></span>
+            <label className='label_form' htmlFor=''>
+              Email
+            </label>
+          </div>
+          <div className='grupo'>
+            <input
+              className='input_form'
+              type='password'
+              name='password'
+              id='passwprd'
+              required
+            />
+            <span className='barra'></span>
+            <label className='label_form' htmlFor=''>
+              Contraseña
+            </label>
+          </div>
+          <div className='boton_form' id='btn_login'>
+            <button type='submit'>Iniciar Sesi&oacute;n</button>
+          </div>
+          <br />
+          <a href=''>¿Se te olvido la contraseña?</a>
+        </div>
+      </form>
+    </div>
+  );
+}
 
-export default LoginUsr
+export default LoginUsr;
--- tis/src/Componentes/Mi_Perfil/perfil.jsx
+++ tis/src/Componentes/Mi_Perfil/perfil.jsx
@@ -1,54 +1,66 @@
-import {Offcanvas} from 'react-bootstrap'
-import {useEffect, useState} from 'react'
-import './perfil.css'
-import { useNavigate } from "react-router-dom";
-import {useAuth} from '../../hooks/useAuth'
+import { Offcanvas } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import './perfil.css';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 function OffCanvasExample({ name, ...props }) {
+  const { logout, user } = useAuth();
+  const [show, setShow] = useState(false);
 
-    const {logout, user} = useAuth()
-    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const navigate = useNavigate();
+  const redirectTo = () => {
+    navigate(`/mis-reservas`);
+  };
 
-    const navigate = useNavigate()
-    const redirectTo = () => {
-      navigate(`/mis-reservas`)        
-  }
-    return (
-      <>
-        <a className="nav-menu-link" variant="primary" style={{cursor:"pointer"}} onClick={handleShow}>
-          Mi perfil
-        </a>
-        <Offcanvas show={show} onHide={handleClose} {...props}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Mi Perfil</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <div className='cont-img-perfil'>
-                <img className="img-perfil" src="/assets/imagenes/perfilpordefecto.png" alt=""/>
-                <p style={{marginTop:12+'px'}}>Nombre del usuario</p>
-                <p>cualquiercorreo@gmail.com</p>
-            </div>
-            <div className='cont-btn-perfil'>
-                <button onClick={()=> logout()}>Cerrar Sesión</button>
-            </div>
-            
-          </Offcanvas.Body>
-        </Offcanvas>
-      </>
-    );
-  }
-  
-  function MiPerfilAdm() {
-    return (
-      <>
-        {[ 'start'].map((placement, idx) => (
-          <OffCanvasExample key={idx} placement={placement} name={placement} />
-        ))}
-      </>
-    );
-  }
-  
-  export default MiPerfilAdm
+  const handleOnClick = () => {
+    navigate('/auth');
+    logout();
+  };
+  return (
+    <>
+      <a
+        className='nav-menu-link'
+        variant='primary'
+        style={{ cursor: 'pointer' }}
+        onClick={handleShow}>
+        Mi perfil
+      </a>
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Mi Perfil</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className='cont-img-perfil'>
+            <img
+              className='img-perfil'
+              src='/assets/imagenes/perfilpordefecto.png'
+              alt=''
+            />
+            <p style={{ marginTop: 12 + 'px' }}>Nombre del usuario</p>
+            <p>cualquiercorreo@gmail.com</p>
+          </div>
+          <div className='cont-btn-perfil'>
+            <button onClick={handleOnClick}>Cerrar Sesión</button>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+function MiPerfilAdm() {
+  return (
+    <>
+      {['start'].map((placement, idx) => (
+        <OffCanvasExample key={idx} placement={placement} name={placement} />
+      ))}
+    </>
+  );
+}
+
+export default MiPerfilAdm;
+
